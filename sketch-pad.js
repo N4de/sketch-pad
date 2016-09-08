26f2ea14551af6d9c0592a2e2d04601d19a969ce
N4de <JaAnnala@Gmail.com>
2016-09-08 16:30:32 +0300

    add reset button

--- sketch-pad.js
+++ sketch-pad.js
@@ -2,18 +2,33 @@ $(document).ready(function() {
 	
 	for (var i = 0; i < 16; i++) {
 		$('table').append('<tr></tr>')
-	}
+	};
 
 	for (var k = 0; k < 16; k++) {
 			$('tr').append('<td><div class="pix"></div></td>');
-		}
+		};
 
 	$('.pix').mouseenter(function() {
 		$(this).addClass('blue');
-	});	
+	});
 
-	var clear = function() {
-		var amount = prompt("Enter the size of grid");
+	document.getElementById("button").onclick = function() {myFunction()};
+
+	function myFunction() {
+  	  var amount = prompt("Enter size of grid");
+  	  $('table').remove();
+  	  $('.container').append('<table></table>');
+
+  	  for (var i = 0; i < amount; i++) {
+				$('table').append('<tr></tr>')
+			};
+
+			for (var k = 0; k < amount; k++) {
+				$('tr').append('<td><div class="pix"></div></td>');
+			};
+
+			$('.pix').mouseenter(function() {
+				$(this).addClass('blue');
+			});
 	}
-
 });
