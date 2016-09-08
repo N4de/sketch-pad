3bdf5fc9042db30ce870cc27e534bf5d138b8089
N4de <JaAnnala@Gmail.com>
2016-09-08 12:06:55 +0300

    add working grid

--- sketch-pad.js
+++ sketch-pad.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-	$('.container').append("<div class='pic'></div>");
-	$('.pic').height("200px");
-	$('.pic').width("200px");
-	$('.pic').css("color", "green");
+	
+	for (var i = 0; i < 16; i++) {
+		$('table').append('<tr></tr>')
+	}
+
+	for (var k = 0; k < 16; k++) {
+			$('tr').append('<td><div class="pix"></div></td>');
+		}
+
+	$('.pix').mouseenter(function() {
+		$(this).addClass('blue');
+	});	
+
+	var clear = function() {
+		var amount = prompt("Enter the size of grid");
+	}
+
 });
